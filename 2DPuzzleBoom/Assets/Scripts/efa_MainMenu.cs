3d3c3d57198c79f9ddbc0f0ab9f991c0bfcbe598
knabberen <eivindfalkenberg@Eivinds-MacBook-Pro.local>
2016-02-28 17:39:59 +0100

    playadded

---
+++ 2DPuzzleBoom/Assets/Scripts/efa_MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class efa_MainMenu : MonoBehaviour
+{
+
+	public Button play;
+
+
+	public void PlayGame ()
+	{
+
+		Application.LoadLevel ("TestLevel");
+	}
+}
