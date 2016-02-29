db4d796f8648f517179340c367076b993e6aeabf
knabberen <eivindfalkenberg@Eivinds-MacBook-Pro.local>
2016-02-29 14:36:14 +0100

    exit trigger

---
+++ 2DPuzzleBoom/Assets/Scripts/LevelTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelTrigger : MonoBehaviour
+{
+
+	void OnCollisionEnter2D (Collision2D col)
+	{
+		if (col.gameObject.tag.Equals ("LevelExit"))
+			SceneManager.LoadScene ("MainMenu");
+	}
+}
