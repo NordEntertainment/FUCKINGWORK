b98b2a65e1777fa211fcab5139c0d38f167f4f6d
knabberen <knabberen@gmail.com>
2016-03-08 01:33:35 +0100

    shootable greenbox

---
+++ 2DPuzzleBoom/Assets/Scripts/Colliders/DestroyBoxCol.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyBoxCol : MonoBehaviour
+{
+
+
+	void OnCollisionEnter2D (Collision2D col)
+	{
+
+		if (col.gameObject.tag.Equals ("Projectile")) {
+
+			Destroy (this.gameObject);
+			//			print ("boom");
+		}
+	}
+}
